0c254d77578f98d51d65c9c550e79eee2c7f87e0
HunterLM <hunter109@gmail.com>
2022-05-09 13:07:23 -0500

    trying to fix module error

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/config');
+const sequelize = require('./config/connection');
 
 class Comment extends Model {}
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes, STRING } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/config');
+const sequelize = require('./config/connection');
 
 // create our User model
 class User extends Model {

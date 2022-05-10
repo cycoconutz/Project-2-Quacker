d965bc699bd7e751965d8b94b1a469d9ceed2600
HunterLM <hunter109@gmail.com>
2022-05-10 08:53:33 -0500

    updated api/comment-routes and api/index so get, post, put, delete, work for comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
 const postRoutes = require("./post-routes");
-// const commentRoutes = require("./comment-routes");
+const commentRoutes = require("./comment-routes");
 
 router.use("/user", userRoutes);
 router.use("/post", postRoutes);
-// router.use("/comment", commentRoutes);
+router.use("/comment", commentRoutes);
 
 
 module.exports = router;

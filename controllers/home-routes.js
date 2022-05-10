93f17f406ac5b963f3aa680ab66bb0ae5fd96fa4
HunterLM <hunter109@gmail.com>
2022-05-10 13:34:05 -0500

    removed unneeded commented out code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -136,21 +136,6 @@ router.get("/user/:id", async (req, res) => {
   }
 });
 
-// // GET one painting
-// // Use the custom middleware before allowing the user to access the painting withAuth,
-// router.get('/painting/:id', async (req, res) => {
-//   try {
-//     const dbPaintingData = await Painting.findByPk(req.params.id);
-
-//     const painting = dbPaintingData.get({ plain: true });
-
-//     res.render('painting', { painting, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");

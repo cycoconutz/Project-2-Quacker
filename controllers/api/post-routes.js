08c2d2b083525c67dd8bd663afe3532c54ee6513
HunterLM <hunter109@gmail.com>
2022-05-11 13:47:53 -0500

    merged with main

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -13,4 +13,46 @@ router.get('/', async (req, res) => {
   }
 });
 
+// CREATE new post
+router.post('/', async (req, res) => {
+  console.log('entered the post req');
+  try {
+    const newPost = await Post.create({
+      attributes: ['username'],
+      include: [{ model: User }],
+      ...req.body,
+    });
+    res.status(200).json(newPost + req.session.username);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// delete post
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletePost = await Post.destroy({
+      where: { id: req.params.id },
+    });
+    res.json(deletePost);
+  } catch (err) {
+    console.log(err);
+    if (err) throw err;
+  }
+});
+
+// Update Post
+router.put('/:id', async (req, res) => {
+  try {
+    const updatePost = await Post.update({
+      where: { id: req.params.id },
+    });
+    res.json(updatePost);
+  } catch (err) {
+    console.log(err);
+    if (err) throw err;
+  }
+});
+
 module.exports = router;

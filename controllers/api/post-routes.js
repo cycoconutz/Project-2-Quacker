493e8f1141626b4abff0ec7b94add4bfdaf31343
John Yates <johndyates@gmail.com>
2022-05-12 12:27:55 -0500

    delete working

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -65,7 +65,7 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const affectedRows = Post.destroy({
+    const deletePost = Post.destroy({
       where: {
         id: req.params.id,
       },
